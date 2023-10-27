4175cf577f10b3ef0a673e22fade510409ad4ec3
max1219 <bescenniy.2003@mail.ru>
2023-10-27 13:50:19 +0300

    Updated ResponseEntity: added List<String> errors

--- src/main/java/entities/responses/ResponseEntity.java
+++ src/main/java/entities/responses/ResponseEntity.java
@@ -1,12 +1,16 @@
 package entities.responses;
 
+import java.util.List;
+
 public class ResponseEntity<T> {
     private final T arg;
     private final short statusCode;
+    private final List<String> errors;
 
-    public ResponseEntity(T arg, short statusCode) {
+    public ResponseEntity(T arg, short statusCode, List<String> errors) {
         this.arg = arg;
         this.statusCode = statusCode;
+        this.errors = errors;
     }
 
     public T getArg() {
@@ -15,5 +19,9 @@ public class ResponseEntity<T> {
 
     public short getStatusCode() {
         return statusCode;
+    }
+
+    public List<String> getErrors() {
+        return errors;
     }
 }
