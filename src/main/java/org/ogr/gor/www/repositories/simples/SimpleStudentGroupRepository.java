21f36a1348e39cada6169de7f2cb207ac99e02ad
max1219 <bescenniy.2003@mail.ru>
2024-05-14 17:27:47 +0400

    Added exceptions throwing into SimpleStudentGroupRepository

--- src/main/java/org/ogr/gor/www/repositories/simples/SimpleStudentGroupRepository.java
+++ src/main/java/org/ogr/gor/www/repositories/simples/SimpleStudentGroupRepository.java
@@ -1,10 +1,14 @@
 package org.ogr.gor.www.repositories.simples;
 
 import org.ogr.gor.www.entities.database.StudentGroup;
+import org.ogr.gor.www.old.exceptions.repository_exceptions.NotFoundException;
 import org.ogr.gor.www.repositories.interfaces.IStudentGroupRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.sql.ResultSet;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -24,22 +28,35 @@ public class SimpleStudentGroupRepository implements IStudentGroupRepository {
     }
 
     @Override
-    public void delete(long id) {
-        jdbcTemplate.update("DELETE FROM student_group WHERE group_id = ?", id);
+    public void delete(long id) throws NotFoundException {
+        int affected = jdbcTemplate.update("DELETE FROM student_group WHERE group_id = ?", id);
+        if (affected == 0) {
+            throw new NotFoundException();
+        }
     }
 
     @Override
-    public void edit(StudentGroup group) {
-        jdbcTemplate.update(
+    public void edit(StudentGroup group) throws NotFoundException {
+        int affected = jdbcTemplate.update(
                 "UPDATE student_group SET group_name = ? WHERE group_id = ?", group.getName(), group.getId());
+        if (affected == 0) {
+            throw new NotFoundException();
+        }
     }
 
     @Override
-    public StudentGroup getById(long id) {
-        return jdbcTemplate.queryForObject("SELECT group_id, group_name FROM student_group WHERE group_id = ?",
-                (resultSet, rowNum) -> new StudentGroup(
-                        resultSet.getLong("group_id"), resultSet.getString("group_name"))
-                , id);
+    public StudentGroup getById(long id) throws NotFoundException {
+        StudentGroup result;
+        try {
+            result = jdbcTemplate.queryForObject("SELECT group_id, group_name FROM student_group WHERE group_id = ?",
+                    (resultSet, rowNum) -> new StudentGroup(
+                            resultSet.getLong("group_id"), resultSet.getString("group_name"))
+                    , id);
+        }
+        catch (EmptyResultDataAccessException e){
+            throw new NotFoundException();
+        }
+        return result;
     }
 
     @Override
