61fb85ca0659fa98ad7b4352929bb9f5700d61d1
max1219 <bescenniy.2003@mail.ru>
2024-05-16 09:19:28 +0400

    Added lessonId to Attendance

--- src/main/java/org/ogr/gor/www/entities/database/Attendance.java
+++ src/main/java/org/ogr/gor/www/entities/database/Attendance.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public class Attendance {
+    private final long lessonId;
     private final long[] visitedId;
 
-    public Attendance(long[] visitedId) {
+    public Attendance(long lessonId, long[] visitedId) {
+        this.lessonId = lessonId;
         this.visitedId = visitedId;
     }
 
@@ -15,16 +17,23 @@ public class Attendance {
         return visitedId;
     }
 
+    public long getLessonId() {
+        return lessonId;
+    }
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Attendance that = (Attendance) o;
-        return Arrays.equals(visitedId, that.visitedId);
+        return lessonId == that.lessonId && Arrays.equals(visitedId, that.visitedId);
     }
 
     @Override
     public int hashCode() {
-        return Arrays.hashCode(visitedId);
+        int result = Objects.hash(lessonId);
+        result = 31 * result + Arrays.hashCode(visitedId);
+        return result;
     }
 }
